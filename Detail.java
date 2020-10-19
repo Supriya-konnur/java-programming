cba6c1d1da0eb7648ed4949be4a082c072ffcdcc
Supriya-konnur <73068126+Supriya-konnur@users.noreply.github.com>
2020-10-19 22:30:59 +0530

    Add files via upload
    groupmeber info

---
+++ Detail.java
@@ -0,0 +1,41 @@
+class Detail{
+public static void main(String args[]){
+
+Group G1 = new Group();
+G1.name ="Supriya";
+G1.branch ="ECE";
+G1.college_name ="BGMIT";
+G1.place="Jamakhandi";
+System.out.println("details of the group members with their respective name, brach, college name and place , is as follows:\n");
+G1.display();
+
+Group G2 = new Group();
+G2.name ="Soubhagya";
+G2.branch ="ECE";
+G2.college_name ="BGMIT";
+G2.place="Savadhatti";
+G2.display();
+
+Group G3 = new Group();
+G3.name ="Sharanabasayya";
+G3.branch ="ECE";
+G3.college_name ="BGMIT";
+G3.place="Mudhol";
+G3.display();
+
+Group G4 = new Group();
+G4.name ="Suma";
+G4.branch ="ECE";
+G4.college_name ="SKIT";
+G4.place="Bangalore";
+G4.display();
+
+Group G5 = new Group();
+G5.name ="Kushala";
+G5.branch ="ISE";
+G5.college_name ="BGSIT";
+G5.place="Mysore";
+G5.display();
+
+}
+}
